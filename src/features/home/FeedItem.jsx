4e3986d4d9ebdeeb11080a64ac2ec80f65ceae20
akak35793-oss <akak4456@naver.com>
2026-04-08 14:28:41 +0900

    [CNG] FeedItem image 구성함

--- src/features/home/FeedItem.jsx
+++ src/features/home/FeedItem.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
+import profileArrowLeft from "../../assets/profile-arrow-left.png";
+import profileArrowRight from "../../assets/profile-arrow-right.png";
 
 const Wrapper = styled.div`
   width: 470px;
-  border: 1px solid #dbdbdb;
   border-radius: 8px;
   background: white;
   margin: 20px auto;
@@ -18,12 +20,38 @@ const Header = styled.div`
 const Left = styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 8px;
+`;
+
+const Ring = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  padding: 2px;
+  background: linear-gradient(
+    45deg,
+    #feda75,
+    #fa7e1e,
+    #d62976,
+    #962fbf,
+    #4f5bd5
+  );
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Profile = styled.div`
-  width: 32px;
-  height: 32px;
+  width: calc(100% - 4px); /* 🔥 핵심 */
+  height: calc(100% - 4px);
   border-radius: 50%;
   background: url(${(p) => p.src}) center/cover;
 `;
@@ -33,14 +61,71 @@ const Username = styled.div`
   font-size: 14px;
 `;
 
+const Time = styled.div`
+  color: #858b92;
+  font-size: 14px;
+`;
+
 const More = styled.div`
   cursor: pointer;
 `;
 
-const ImageWrapper = styled.div`
+const ImageContainer = styled.div`
+  position: relative;
   width: 100%;
-  height: 470px;
-  background: url(${(p) => p.src}) center/cover;
+  overflow: hidden;
+  border-radius: 8px;
+`;
+
+const Slider = styled.div`
+  display: flex;
+  transform: translateX(${(p) => `-${p.index * 100}%`});
+  transition: transform 0.3s ease;
+`;
+
+const Slide = styled.img`
+  min-width: 100%;
+  height: auto;
+  object-fit: cover;
+`;
+
+const Arrow = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  ${({ left }) => (left ? "left: 10px;" : "right: 10px;")}
+
+  width: 32px;
+  height: 32px;
+
+  background: white;
+  border: none;
+  border-radius: 50%;
+
+  display: flex; /* 🔥 추가 */
+  align-items: center; /* 🔥 세로 중앙 */
+  justify-content: center; /* 🔥 가로 중앙 */
+
+  z-index: 10;
+
+  cursor: pointer;
+  opacity: 0.7;
+`;
+
+const Dots = styled.div`
+  position: absolute;
+  bottom: 16px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 6px;
+`;
+
+const Dot = styled.div`
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background: ${({ active }) => (active ? "white" : "#ccc")};
 `;
 
 const Actions = styled.div`
@@ -78,19 +163,76 @@ const Comments = styled.div`
 `;
 
 const FeedItem = ({ post }) => {
+  console.log(post);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const total = post.images.length;
+
+  const prev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? prev : prev - 1));
+  };
+
+  const next = () => {
+    setCurrentIndex((prev) => (prev === total - 1 ? prev : prev + 1));
+  };
+  const getTimeDiff = (createdAt) => {
+    const now = new Date();
+    const created = new Date(createdAt);
+
+    const diff = Math.floor((now - created) / 1000); // 초 단위
+
+    if (diff < 60) return `${diff}초`;
+    if (diff < 3600) return `${Math.floor(diff / 60)}분`;
+    if (diff < 86400) return `${Math.floor(diff / 3600)}시간`;
+    if (diff < 604800) return `${Math.floor(diff / 86400)}일`;
+
+    return `${Math.floor(diff / 604800)}주`;
+  };
   return (
     <Wrapper>
       {/* Header */}
       <Header>
         <Left>
-          <Profile src={post.profileImage} />
-          <Username>{post.username}</Username>
+          <Ring>
+            <Inner>
+              <Profile src={post.user.profileImage} />
+            </Inner>
+          </Ring>
+          <Username>{post.user.username}</Username>
+          <Time>• {getTimeDiff(post.createdAt)}</Time>
         </Left>
         <More>•••</More>
       </Header>
 
       {/* Image */}
-      <ImageWrapper src={post.image} />
+      <ImageContainer>
+        <Slider index={currentIndex}>
+          {post.images.map((img, idx) => (
+            <Slide key={idx} src={img} />
+          ))}
+        </Slider>
+
+        {/* 좌우 버튼 */}
+        {currentIndex > 0 && (
+          <Arrow left onClick={prev}>
+            <img src={profileArrowLeft} alt="profile-arrow-left" />
+          </Arrow>
+        )}
+        {currentIndex < total - 1 && (
+          <Arrow onClick={next}>
+            <img src={profileArrowRight} alt="profile-arrow-right" />
+          </Arrow>
+        )}
+
+        {/* Dot */}
+        {total > 1 && (
+          <Dots>
+            {post.images.map((_, idx) => (
+              <Dot key={idx} active={idx === currentIndex} />
+            ))}
+          </Dots>
+        )}
+      </ImageContainer>
 
       {/* Actions */}
       <Actions>
--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -5,29 +5,6 @@ export const posts = [
     images: [
       "https://images.unsplash.com/photo-1773332585687-85beb4da71ab?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1683129807314-95150b5c3fb1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ],
-    caption: "오늘 날씨 좋다 ☀️",
-    likeCount: 12,
-    commentCount: 3,
-    createdAt: "2026-04-06T12:00:00",
-  },
-  {
-    id: 2,
-    userId: "01012341234",
-    images: [
-      "https://images.unsplash.com/photo-1773332585687-85beb4da71ab?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://plus.unsplash.com/premium_photo-1683129807314-95150b5c3fb1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ],
-    caption: "오늘 날씨 좋다 ☀️",
-    likeCount: 12,
-    commentCount: 3,
-    createdAt: "2026-04-06T12:00:00",
-  },
-  {
-    id: 3,
-    userId: "01012341234",
-    images: [
-      "https://images.unsplash.com/photo-1773332585687-85beb4da71ab?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "https://plus.unsplash.com/premium_photo-1683129807314-95150b5c3fb1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     ],
     caption: "오늘 날씨 좋다 ☀️",
