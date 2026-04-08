b62c2a06b6db9edd727da718a9857e6e463611ad
akak35793-oss <akak4456@naver.com>
2026-04-08 16:25:58 +0900

    [CNG] Modal box 구성함

--- src/components/modal/ReplyModal.jsx
+++ src/components/modal/ReplyModal.jsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom";
 import styled from "styled-components";
+import replyModalX from "../../assets/reply-modal-x.png";
 
 const Overlay = styled.div`
   position: fixed;
@@ -10,12 +11,36 @@ const Overlay = styled.div`
   align-items: center;
   z-index: 999;
 `;
+const ReplyModalXDiv = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  img {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+    cursor: pointer;
+  }
+`;
+
+const ModalBox = styled.div`
+  position: fixed;
+  width: 60%;
+  height: 95%;
+  background-color: white;
+`;
 
 const ReplyModal = ({ open, onClose }) => {
   if (!open) return null;
 
   return createPortal(
-    <Overlay onClick={onClose}></Overlay>,
+    <Overlay onClick={onClose}>
+      <ReplyModalXDiv>
+        <img src={replyModalX} alt="reply-modal-x" />
+      </ReplyModalXDiv>
+      <ModalBox onClick={(e) => e.stopPropagation()}></ModalBox>
+    </Overlay>,
     document.getElementById("modal-root"),
   );
 };
