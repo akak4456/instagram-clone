379fe3d8f7a8de038f055b25e4e5139f3cb882bc
akak35793-oss <akak4456@naver.com>
2026-04-10 11:38:59 +0900

    [CNG] storymodal top 구성함

--- src/features/story/StoryModal.jsx
+++ src/features/story/StoryModal.jsx
@@ -1,25 +1,35 @@
 import { createPortal } from "react-dom";
 import styled from "styled-components";
 import storyModalX from "../../assets/story-modal-x.png";
+import storyInstagramLogo from "../../assets/story-instagram-logo.png";
 
 const Overlay = styled.div`
   position: fixed;
   inset: 0;
   background: #1a1a1a;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   z-index: 999;
 `;
 const ModalXDiv = styled.div`
   width: 100vw;
   height: 100vh;
-  position: relative;
+  position: absolute;
+  inset: 0;
   img {
     position: absolute;
-    top: 8px;
-    right: 8px;
+    top: 24px;
+    right: 24px;
     cursor: pointer;
+  }
+`;
+const StoryInstagramLogoDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  inset: 0;
+  img {
+    position: absolute;
+    top: 24px;
+    left: 24px;
   }
 `;
 
@@ -27,6 +37,9 @@ const StoryModal = ({ open, onClose, post }) => {
   if (!open) return null;
   return createPortal(
     <Overlay>
+      <StoryInstagramLogoDiv>
+        <img src={storyInstagramLogo} alt="story-instagram-logo" />
+      </StoryInstagramLogoDiv>
       <ModalXDiv>
         <img src={storyModalX} alt="reply-modal-x" onClick={onClose} />
       </ModalXDiv>
