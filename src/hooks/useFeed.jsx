fdcc5c05edf019762902328a9fcdbf7a1eb5a55b
akak35793-oss <akak4456@naver.com>
2026-04-16 12:45:07 +0900

    [CNG] useFeed 수정함

--- src/hooks/useFeed.jsx
+++ src/hooks/useFeed.jsx
@@ -3,6 +3,12 @@ import { FeedContext } from "../contexts/FeedContext";
 
 export const useFeed = (userId) => {
   const context = useContext(FeedContext);
+  if (!context) {
+    throw new Error("useFeed는 FeedProvider 내부에서 사용해야 합니다.");
+  }
+  if (!userId) {
+    throw new Error("useFeed는 userId를 받아야 합니다");
+  }
   const { posts, loadPosts } = context;
   useEffect(() => {
     if (posts.length === 0) {
