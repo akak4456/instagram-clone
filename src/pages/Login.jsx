1c605a3f3d36f90ad21f080dcbac0527c8a9c92c
akak35793-oss <akak4456@naver.com>
2026-04-16 15:02:38 +0900

    [CNG] login 페이지에 resetPosts 호출하도록 변경함

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import LoginTop from "../features/login/LoginTop";
 import LoginForm from "../features/login/LoginForm";
 import LoginBottom from "../features/login/LoginBottom";
+import { usePost } from "../hooks/usePost";
 
 const Login = () => {
   const [error, setError] = useState("");
+  const { resetPosts } = usePost();
   const { login } = useAuth();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    resetPosts();
+  }, []);
 
   const handleLogin = async (form) => {
     const res = await login(form);
